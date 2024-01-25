f3f981f61f24793058da87ea33861525ff94234c
KvaskoVladimir <Kvasko-V@yandex.ru>
2024-01-25 23:28:00 +0400

    добавить простой интерфейс рисования линий на доске

---
+++ ChatHub.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.SignalR;
+ 
+namespace SignalRApp
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string message)
+        {
+            await this.Clients.All.SendAsync("Receive", message);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
+using SignalRApp;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();
+
 var app = builder.Build();
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
 //app.MapGet("/", () => "Hello World!");
+
+app.MapHub<ChatHub>("/chat");
 
 app.Run();
