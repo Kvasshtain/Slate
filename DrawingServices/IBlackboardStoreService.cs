a50ff3622ed393ad227d6e934b1b2561b3f5a27d
kvasshtain <kvasko-v@yandex.ru>
2024-05-07 23:31:18 +0400

    Добавлено удаление объектов

--- DrawingServices/IBlackboardStoreService.cs
+++ DrawingServices/IBlackboardStoreService.cs
@@ -2,8 +2,9 @@ namespace DrawingServices
 {
     public interface IBlackboardStoreService
     {
-        IEnumerable<BlackboardObjectData> BlackboardObjects {get;}
+        IEnumerable<BlackboardObjectData> BlackboardObjects { get; }
         bool TryAddObject(BlackboardObjectData blackboardObjectData);
+        bool TryDeleteObjectsByIds(string[] deletedFromCanvasObjectsIds);
         void DragObject(DragObjectData dragObjectData);
         void ScaleObject(ScaleObjectData scaleObjectData);
         void RotateObject(RotateObjectData rotateObjectData);
--- ImageHub.cs
+++ ImageHub.cs
@@ -1,16 +1,17 @@
-using Microsoft.AspNetCore.SignalR;
-using DrawingServices;
 using System.Diagnostics;
+using DrawingServices;
+using Microsoft.AspNetCore.SignalR;
 
 namespace SignalRApp
 {
     public class ImageHub(IBlackboardStoreService imageStoreService) : Hub
     {
-        private readonly IBlackboardStoreService objectStoreService = imageStoreService ?? throw new ArgumentNullException(nameof(imageStoreService));
+        private readonly IBlackboardStoreService objectStoreService =
+            imageStoreService ?? throw new ArgumentNullException(nameof(imageStoreService));
 
         public async Task GetAllBoardObjects()
         {
-            foreach(var boardObject in objectStoreService.BlackboardObjects)
+            foreach (var boardObject in objectStoreService.BlackboardObjects)
             {
                 await Clients.Caller.SendAsync("AddObjectOnCanvas", boardObject);
 
@@ -27,9 +28,23 @@ namespace SignalRApp
 
                 return;
             }
-            
+
             await Clients.Caller.SendAsync("AddObjectError", boardObject);
             Debug.WriteLine("Create object error");
+        }
+
+        public async Task DeleteObjectsByIds(string[] deletedObjectsIds)
+        {
+            if (objectStoreService.TryDeleteObjectsByIds(deletedObjectsIds))
+            {
+                await Clients.All.SendAsync("DeleteObjectsOnCanvas", deletedObjectsIds);
+                Debug.WriteLine("Delete objects by Ids");
+
+                return;
+            }
+
+            await Clients.Caller.SendAsync("DeleteObjectsError", deletedObjectsIds);
+             
         }
 
         public async Task Drag(DragObjectData payload)
@@ -40,7 +55,7 @@ namespace SignalRApp
 
             Debug.WriteLine("Drag object");
         }
-    
+
         public async Task Scale(ScaleObjectData payload)
         {
             objectStoreService.ScaleObject(payload);
